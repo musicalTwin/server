44d95769913e750b3c6d6f37c8bd026107b4c4d0
aleeeee1 <alexfapper.2005@gmail.com>
2022-09-21 22:15:11 +0200

    Add postget for getting data form frontend

--- src/main/java/it/musicaltwin/demo/controllers/UsersGenresController.java
+++ src/main/java/it/musicaltwin/demo/controllers/UsersGenresController.java
@@ -1,19 +1,21 @@
 package it.musicaltwin.demo.controllers;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import it.musicaltwin.demo.entities.UsersGenres;
 import it.musicaltwin.demo.services.UsersGenresService;
 
-
 @RestController
-@RequestMapping(path = "api/v1/user-listened-genres")
+@RequestMapping(path = "api/v1/user-genres")
 public class UsersGenresController {
 
     @Autowired
@@ -26,6 +28,15 @@ public class UsersGenresController {
     @GetMapping(path = "{userId}")
     public List<UsersGenres> getListenedGenres(@PathVariable("userId") String userId) {
         return usersGenresService.getListenedGenres(userId);
+    }
+
+    @PostMapping
+    public void addListenedGenres(@RequestBody Map<String, List<String>> body) {
+        String id = body.get("id").get(0);
+        List<String> genres = body.get("genres");
+
+        genres.forEach(null);
+        System.out.println(id);
     }
 
 }
